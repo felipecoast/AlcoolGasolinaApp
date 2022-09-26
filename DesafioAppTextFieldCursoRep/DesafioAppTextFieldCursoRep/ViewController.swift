eff5dea0daf68e44e1d609877d9c2a89d65573f3
felipecoast <felipe.coast@gmail.com>
2022-09-26 20:45:10 -0300

    IBOutlet created

--- DesafioAppTextFieldCursoRep/DesafioAppTextFieldCursoRep/ViewController.swift
+++ DesafioAppTextFieldCursoRep/DesafioAppTextFieldCursoRep/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var appNameLabel: UILabel!
+    @IBOutlet weak var gasolineAlcoolLabel: UILabel!
+    
+    @IBOutlet weak var alcoolTextField: UITextField!
+    @IBOutlet weak var gasolineTextField: UITextField!
+    
+    @IBOutlet weak var calculateButton: UIButton!
+    @IBOutlet weak var writePricesButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
 
 }
 
