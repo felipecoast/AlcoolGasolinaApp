d03a995f3152247ffb0cfb087f5a534b9385eb96
felipecoast <felipe.coast@gmail.com>
2022-09-26 21:05:08 -0300

    Add configButton, configTextField and configLabel

--- DesafioAppTextFieldCursoRep/DesafioAppTextFieldCursoRep/ViewController.swift
+++ DesafioAppTextFieldCursoRep/DesafioAppTextFieldCursoRep/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var appNameLabel: UILabel!
     @IBOutlet weak var gasolineAlcoolLabel: UILabel!
@@ -20,7 +20,33 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemGreen
+        configLabel()
+        configTextField()
+        configButton()
+    }
+    
+    func configLabel() {
+        appNameLabel.text = "Álcool OU Gasolina"
+        gasolineAlcoolLabel.text = "     Preço Álcool           Preço Gasolina"
+    }
+    
+    func configTextField() {
+        alcoolTextField.placeholder = "Ex: 1.99"
+        gasolineTextField.placeholder = "Ex: 2.99"
+        
+        alcoolTextField.delegate = self
+        gasolineTextField.delegate = self
+    }
+    
+    func configButton() {
+        calculateButton.setTitle("Calcular", for: .normal)
+        calculateButton.backgroundColor = .blue
+        calculateButton.tintColor = .white
+        
+        writePricesButton.setTitle("Digite os Preços!", for: .normal)
+        writePricesButton.backgroundColor = .blue
+        writePricesButton.tintColor = .white
     }
 
     @IBAction func tappedCalculateButton(_ sender: UIButton) {
